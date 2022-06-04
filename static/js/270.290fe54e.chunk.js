50c2918194cf34268417891ae7325f1bbb7d078c
Anthony Wong <nakagame02@gmail.com>
2022-06-04 23:01:34 +0900

    Updates

---
+++ static/js/270.290fe54e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshinoreact=self.webpackChunkshinoreact||[]).push([[270],{270:function(e,n,t){t.r(n),t.d(n,{DetailPage:function(){return s}});var i=t(504),c=t(184),s=function(){return(0,c.jsxs)("div",{className:"d-flex flex-column justify-content-center align-items-center",children:[(0,c.jsx)("div",{children:"this is the detail page"}),(0,c.jsx)(i.rU,{to:"/",children:"go back to home"})]})}}}]);
+//# sourceMappingURL=270.290fe54e.chunk.js.map
