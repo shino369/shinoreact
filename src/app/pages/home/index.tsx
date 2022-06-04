01708ac4edb6bc8a6eba7edf53691eb86b7b3904
Anthony Wong <nakagame02@gmail.com>
2022-06-05 01:19:12 +0900

    feat: add image

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,18 +1,49 @@
 // import { useCallback, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 export const HomePage = () => {
-
   return (
     <div className="p-4">
       <div className="page-header mb-2">
         <strong>Welcome to SHINO REACT</strong>
       </div>
-      <p>This page is a personal page host in Github Page.</p>
-      <p>Made with react, and simply use as a resume for <span className="high-light">job seeking</span>.</p>
-      <p className="remark">{'( Perhaps there maybe further development or features in the future )'}</p>
+      <div className="">
+        <p>This page is a personal page host in Github Page.</p>
+        <p>
+          Made with react, simply use as a demo for 
+          <span className="ms-1 high-light">job seeking</span>.
+        </p>
+        <p
+          className="remark"
+          style={{
+            fontSize: "0.8rem",
+          }}
+        >
+          {
+            "( Perhaps there maybe further development or features in the future. No one knows. )"
+          }
+        </p>
+        <p className="opactiy-5">
+          If you are using mobile device, please click the menu icon to toggle nav sidebar
+        </p>
+      </div>
+
+      <hr />
+
+      <div className="d-flex justify-content-center">
+        <img
+          width={"100%"}
+          height={"auto"}
+          style={{
+            maxWidth: "500px",
+          }}
+          src="https://c.tenor.com/a5MVWtIRNFUAAAAC/genba-neko.gif"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
